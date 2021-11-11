d3e8f1dbb73f1ec15464f0e95df11f1f5ecd738e
__Oblivion__ <88514898+TristanWellman@users.noreply.github.com>
2021-11-11 10:57:43 -0500

    Add files via upload

---
+++ Raspbian/color.h
@@ -0,0 +1,32 @@
+//Regular text
+#define BLACK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+#define PURPLE "\e[0;35m"
+#define CYAN "\e[0;36m"
+#define WHITE "\e[0;37m"
+
+//Regular bold text
+#define BBLACK "\e[1;30m"
+#define BRED "\e[1;31m"
+#define BGREEN "\e[1;32m"
+#define BYELLOW "\e[1;33m"
+#define BBLUE "\e[1;34m"
+#define BPURPLE "\e[1;35m"
+#define BCYAN "\e[1;36m"
+#define BWHITE "\e[1;37m"
+
+//Regular underline text
+#define UBLAK "\e[4;30m"
+#define URED "\e[4;31m"
+#define UGREEN "\e[4;32m"
+#define UYELLOW "\e[4;33m"
+#define UBLUE "\e[4;34m"
+#define UPURPLE "\e[4;35m"
+#define UCYAN "\e[4;36m"
+#define UWHITE "\e[4;37m"
+
+//Reset
+#define RESET "\e[0m"
